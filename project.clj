c8df37ecca19f4c04efacb2f2e2d6242ce583f3e
Nick DePalma <ndepalma@mit.edu>
2016-09-24 14:52:33 -0500

    This version

--- project.clj
+++ project.clj
@@ -9,23 +9,26 @@
   :plugins [[lein-localrepo "0.5.3"]
             [lein-ring "0.8.6"]]
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojars.ghoseb/mongo-java-driver "1.1"]
+  ;; :dev-dependencies [
+  ;;           [org.clojure/clojure "1.5.1"]]
+  :dependencies [
+                 ;; [org.clojure/clojure "1.5.1"]
+                 ;; [org.clojars.ghoseb/mongo-java-driver "1.1"]
                  [org.clojars.zaxtax/java-aws-mturk "1.6.2"]
                  ;; [org.python/jython-standalone "2.5.3"]
                  ]
 
   :repositories {"local" ~(str (.toURI (java.io.File. "lib")))}
-  ;;:main org.python.util.jython
+  ;; :main org.python.util.jython
   :java-source-paths ["src/crowd"
                       "src/utils"]
-  :source-paths ["scripts/clojure/" ]
-  :target-path "target/"
+  ;; :source-paths ["scripts/clojure/" ]
+  ;; :target-path "target/"
   ;; Directory in which to place AOT-compiled files. Including %s will
   ;; splice the :target-path into this value.
   :compile-path "target/classes"
-  :auto-clean true
-  :aot :all
-  :omit-source true
+  ;; :auto-clean true
+  ;; :aot :all
+  ;; :omit-source true
 
   )
